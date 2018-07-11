9bfa9728c43cab88283ff51b7f6d1d1d80415a24
yizreelamerico <yizreeldelvallegonzalez@gmail.com>
2018-07-10 22:13:48 -0800

    footer

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -5,7 +5,7 @@ export default class footer extends Component {
     render() {
         return (
             <div>
-                <Footer copyrights="copy 2018 "
+                <Footer copyrights="copy 2018 Eatable"
                     className='black'>
                 </Footer>
             </div>
