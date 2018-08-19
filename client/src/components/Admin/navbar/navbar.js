747f9b33c5484e2d66667628b4060ab3b8054378
yizreelamerico <32603836+yizreelamerico@users.noreply.github.com>
2018-08-19 12:02:44 -0700

    ET-314FrontReports

--- client/src/components/Admin/navbar/navbar.js
+++ client/src/components/Admin/navbar/navbar.js
@@ -8,9 +8,9 @@ import PropTypes from 'prop-types';
 
 class NavBarAdmin extends Component {
   render() {
-    const  role  = this.props.auth.user.role
+    const role = this.props.auth.user.role
 
-
+    if (role === 'all') {
       return (
         <div className="navmenu">
           <Row>
@@ -18,118 +18,63 @@ class NavBarAdmin extends Component {
               <Parallax className='header' imageSrc="https://source.unsplash.com/collection/281387/900x200" />
             </div>
             <Navbar className="nabAdmin">
-              <Link to="/General">
-                <div className="contenedor" id="uno">
-                  <Row>
-                    <Col>
-                      <img class="icon" src="assets/general.svg" />
-                    </Col>
-                    <Col>
-                      <p className="texto">General</p>
-                    </Col>
-                  </Row>
-                </div>
-              </Link>
-  
-              <Link to="/User">
-                <div className="contenedor" id="dos">
-                  <Row>
-                    <Col>
-                      <img class="icon" src="assets/boy.svg" />
-                    </Col>
-                    <Col>
-                      <p className="texto">Users</p>
-                    </Col>
-                  </Row>
-                </div>
-              </Link>
-  
-              <Link to="/InventoryView">
-              <div className="contenedor" id="tres">
-                <Row>
-                  <Col>
-                    <img class="icon" src="assets/warehouse.svg" />
-                  </Col>
-                  <Col>
-                    <p className="texto">Inventory</p>
-                  </Col>
-                </Row>
-              </div>
-              </Link>
-  
-              <div className="contenedor" id="cuatro">
-                <Row>
-                  <Col>
-                    <img class="icon" src="assets/cashier.svg" />
-                  </Col>
-                  <Col>
-                    <p className="texto">Casher</p>
-                  </Col>
-                </Row>
-              </div>
-  
-  
-              <Link to="/ReportsView">
-  
-                <div className="contenedor" id="cinco">
-                  <Row>
-                    <Col>
-                      <img class="icon" src="assets/report.svg" />
-                    </Col>
-                    <Col>
-                      <p className="texto">Reports</p>
-                    </Col>
-                  </Row>
-                </div>
-              </Link>
-  
-              <Link to="/addProduct" >
-                <div className="contenedor" id="seis">
-                  <Row>
-                    <Col>
-                      <img class="icon" src="assets/menu.svg" />
-                    </Col>
-                    <Col>
-                      <p className="texto">Add Product</p>
-                    </Col>
-                  </Row>
-                </div>
-  
-              </Link>
-              <Link to="/addIngredient" >
-                <div className="contenedor" id="seven">
-                  <Row>
-                    <Col>
-                      <img class="icon" src="assets/menu.svg" />
-                    </Col>
-                    <Col>
-                      <p className="texto">Add ingredient</p>
-                    </Col>
-                  </Row>
-                </div>
+              <NavItem className="conteinernav" componentClass={Link} href="/General" to="/General">
+                <img className='menu-icon iconnav' src='assets/general.svg' width='31px' />
+                General
+            </NavItem>
 
-              </Link>
-  
-  
-              <div className="login right" id="uno">
-                <Row>
-                  <Col>
-                    <img class="icon" src="assets/stand-by.svg" />
-                  </Col>
-                </Row>
-              </div>
+              <NavItem className="conteinernav" componentClass={Link} href="/User" to="/User">
+                <img className='menu-icon iconnav' src='assets/boy.svg' width='31px' />
+                Users
+            </NavItem>
+
+              <NavItem className="conteinernav" componentClass={Link} href="/InventoryView" to="/InventoryView">
+                <img className='menu-icon iconnav' src='assets/warehouse.svg' width='31px' />
+                Inventory
+            </NavItem>
+
+              <NavItem className="conteinernav" componentClass={Link} href="/cashier" to="/cashier">
+                <img className='menu-icon iconnav' src='assets/cashier.svg' width='31px' />
+                Cashier
+            </NavItem>
+
+              <NavItem className="conteinernav" componentClass={Link} href="/ReportsView" to="/ReportsView">
+                <img className='menu-icon iconnav' src='assets/report.svg' width='31px' />
+                Reports
+            </NavItem>
+
+              <NavItem className="conteinernav" componentClass={Link} href="/addProduct" to="/addProduct">
+                <img className='menu-icon iconnav' src='assets/menu.svg' width='30px' />
+                Add Product
+            </NavItem>
+
+              <NavItem className="conteinernav" componentClass={Link} href="/addIngredient" to="/addIngredient">
+                <img className='menu-icon iconnav' src='assets/harvest.svg' width='30px' />
+                Add Ingredient
+            </NavItem>
+
+              <NavItem className="conteinernav right" componentClass={Link} href="/General" to="/General">
+                <img className='menu-icon iconnav' src='assets/stand-by.svg' width='30px' />
+                Logout
+            </NavItem>
             </Navbar>
           </Row>
         </div >
       )
+    }
+    else {
+      return (
+        <h1>No se puede mi joven</h1>
+      )
+    }
   }
 }
 
-NavBarAdmin.propTypes={
+NavBarAdmin.propTypes = {
   auth: PropTypes.object.isRequired
 }
 
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
   auth: state.auth
 })
 
