1478eab43eb2d205435d39c0c6c3a68331983fb2
yizreelamerico <32603836+yizreelamerico@users.noreply.github.com>
2018-08-19 13:08:24 -0700

    ET-314FrontReports Permisos

--- client/src/components/Admin/Inventory/Inventoryview.js
+++ client/src/components/Admin/Inventory/Inventoryview.js
@@ -4,26 +4,47 @@ import Col from '../../../../../node_modules/react-materialize/lib/Col';
 import NavBarAdmin from '../navbar/navbar';
 import Inventory from './Inventory';
 /* import './Reports.css' */
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class InventoryView extends Component {
+ class InventoryView extends Component {
     render() {
-        return (
-            <div>
+        const role = this.props.auth.user.role
+        if (role === 'all') {
+            return (
                 <div>
-                    <Row>
-                        <NavBarAdmin />
-                    </Row>
-                    <Row >
-                        <Col m={12} >
-                            <Inventory />
-                        </Col>
-                    </Row>
-                    <Row >
-                        <Col m={12} >
-                        </Col>
-                    </Row>
+                    <div>
+                        <Row>
+                            <NavBarAdmin />
+                        </Row>
+                        <Row >
+                            <Col m={12} >
+                                <Inventory />
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col m={12} >
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        else {
+            console.log(role)
+            return (
+                <h1>No se puede mi joven</h1>
+            )
+        }
     }
 }
+
+InventoryView.propTypes = {
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps)(InventoryView)
